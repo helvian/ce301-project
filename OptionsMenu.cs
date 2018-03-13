d072e31a5f6e1d9c307f4bd0191f599ff5878f0c
helvian <hsande@essex.ac.uk>
2018-03-13 12:03:43 +0000

    Commit - Main Menu Scripts
    Added main menu and transition scripts.

---
+++ OptionsMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+/*
+ * Changes certain values in the option menu
+ */
+
+public class OptionsMenu : MonoBehaviour {
+
+	public AudioMixer au;
+
+	//change the music volume based on the music slider's position
+	public void setMusicVol (float vol) {
+		au.SetFloat ("MusicVol", vol);
+	}
+
+	//change the music volume based on the sound slider's position
+	public void setSoundVol (float vol) {
+		au.SetFloat ("SoundVol", vol);
+	}
+
+	//change the quality based on the quality dropdown
+	public void setQuality (int quality) {
+		QualitySettings.SetQualityLevel (quality);
+	}
+		
+}
